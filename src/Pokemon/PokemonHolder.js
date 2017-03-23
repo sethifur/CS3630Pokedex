561b50379bfb9535236114c0bd0a07c6ae99077e
sethifur <sethjohns@mail.weber.edu>
2017-03-22 20:22:30 -0600

    added pokemon large and small sprites and stub files

---
+++ src/Pokemon/PokemonHolder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import pokemon from './pokemon.js';
+
+class PokemonHolder extends React.Component{
+
+
+
+
+  render(){
+    return(
+        <pokemon />
+    );
+  }
+}
+export default PokemonHolder;
---
+++ src/Pokemon/pokemon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class pokemon extends React.Component{
+
+
+
+
+
+  render(){
+    return(
+        <p>hello </p>
+    );
+  }
+}
+export default pokemon;

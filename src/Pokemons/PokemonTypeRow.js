7a1f1d78b14a00d4f8a5bea062266f5a1e89c54c
sethifur <sethjohns@mail.weber.edu>
2017-04-17 23:34:06 -0600

    images changed

--- src/Pokemons/PokemonTypeRow.js
+++ src/Pokemons/PokemonTypeRow.js
@@ -3,6 +3,6 @@ import './PokemonHolder.css';
 
 export default (props)=>(
   <div className="type">
-       <img src={'/static/images/types/' + props.name + '.png'}/>
+       <img src={'/CS3630Pokedex/static/images/types/' + props.name + '.png'}/>
   </div>
 );

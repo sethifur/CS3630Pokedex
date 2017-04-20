0c2c3d7ba18f5a0adcfcd48834845fb3206ef628
mmerrett89 <mmerrett89@hotmail.com>
2017-04-19 20:46:57 -0600

    final from mike

--- src/App.js
+++ src/App.js
@@ -1,43 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Pokemon from './Pokemon';
 import Item from './Item';
 import Move from './Move';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link, hashHistory, IndexRoute  } from 'react-router';
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.onNavClick = this.onNavClick.bind(this);
   }
 
-  onNavClick(id){
+  onNavClick(id) {
     console.log(id);
   }
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>PokeDex</h2>
-        </div>
-        <div className="App-navigation">
-          <div>
-            <Link to='/Pokemon' activeClassName="active">Pokemon</Link>
+      <div className="container">
+        <div className="App">
+          <div className="App-header">
+            <h2>PokeDex</h2>
           </div>
-          <div>
-            <Link to='/Move' activeClassName="active">Moves</Link>
-          </div>
-          <div>
-            <Link to='/Item' activeClassName="active">Items</Link>
-          </div>
+          <header className="header">
+            <div className="constrain">
+              <nav className="navigation">
+                <a className="nav-toggle" href="#">
+                  <span className="trigger">&#9776;</span>
+                  <ul>
+                    <li><Link to='/Pokemon' activeClassName="active">Pokemon</Link></li>
+                    <li><Link to='/Move' activeClassName="active">Moves</Link></li>
+                    <li><Link to='/Item' activeClassName="active">Items</Link></li>
+                  </ul>
+                </a>
+              </nav>
+            </div>
+          </header>
         </div>
         <div className="pokeScroll">
-        {this.props.children}
+          {this.props.children}
         </div>
       </div>
+
     );
   }
 
